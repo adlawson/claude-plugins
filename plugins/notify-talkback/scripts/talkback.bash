1692eb9999f767f4ce687fe45e41d8c0f7d23e19
adlawson <adlawson@gmail.com>
2026-02-23 13:29:56 +0000

    Update talkback script

--- plugins/notify-talkback/scripts/talkback.bash
+++ plugins/notify-talkback/scripts/talkback.bash
@@ -2,16 +2,16 @@
 set -eo pipefail
 
 if [[ $(uname -s) -ne "Darwin" ]]; then exit 1; fi
+if ! [ -x "$(command -v jq)" ]; then exit 1; fi
 
-input=$(jq -r '.' < /dev/stdin)
 working_directory=$(basename "${CLAUDE_PROJECT_DIR:-$PWD}")
 
 declare instruction
-case $(echo "${input}" | jq -r '.hook_event_name') in
+case $(jq -r '.hook_event_name') in
     Notification) instruction="is waiting";;
     PermissionRequest) instruction="has a question";;
     Stop) instruction="is done";;
-    *) echo $(echo "${input}" | jq -r '.hook_event_name')
+    *) exit 0;;
 esac
 
 say --rate=200 <<-EOF
